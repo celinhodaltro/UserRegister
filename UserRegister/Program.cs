f5bc243d3b9bb03a39c7b0200a86050a7fe5127b
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2025-05-07 17:16:26 -0300

    adicionar lista de usuarios

--- UserRegister/Program.cs
+++ UserRegister/Program.cs
@@ -1,5 +1,10 @@
-﻿internal class Program
+﻿
+
+using UserRegister.Entities;
+
+internal class Program
 {
+    static List<User> users = new List<User>();
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
