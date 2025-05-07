ae6ba020f9047edcc52edbd96f90e14c1557001a
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2025-05-07 17:26:36 -0300

    Criar escopo do programa

--- UserRegister/Program.cs
+++ UserRegister/Program.cs
@@ -7,6 +7,56 @@ internal class Program
     static List<User> users = new List<User>();
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ExecuteProgram();
+    }
+
+    private static bool ExecuteProgram()
+    {
+        bool isRunning = true;
+        while (isRunning)
+        {
+            var option = HandleOptions();
+            switch (option)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+
+                    break;
+                case 0:
+                    isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+
+        return true;
+    }
+
+    private static int HandleOptions()
+    {
+        Console.Clear();
+        Console.WriteLine("\n=== MENU ===");
+        Console.WriteLine("1 - Cadastrar Usuário");
+        Console.WriteLine("2 - Listar Usuários");
+        Console.WriteLine("3 - Buscar Usuário por Nome");
+        Console.WriteLine("0 - Sair");
+        Console.Write("Escolha uma opção: ");
+
+        var input = Console.ReadLine();
+        
+        if(int.TryParse(input, out int option))
+        {
+            return option;
+        }
+        else
+        {
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            return HandleOptions();
+        }
     }
 }
