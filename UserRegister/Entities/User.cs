2ac3591ded5cb86a2401da00f4889cbe48e84c42
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2025-05-07 17:14:37 -0300

    Criar entidade de User

---
+++ UserRegister/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegister.Entities
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Name}, Email: {Email}, Idade: {Age}";
+        }
+    }
+}
